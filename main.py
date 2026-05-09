c9c4493cf8b21df6eae740ff252e6a43db9a4986
KoalAren <160577811+KoalAren@users.noreply.github.com>
2026-05-09 03:57:55 +0300

    Update main.py
    a

--- main.py
+++ main.py
@@ -47,10 +47,6 @@ app = FastAPI(title="Pera Kafe API")
 app.add_middleware(CORSMiddleware, allow_origins=["*"],
                    allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 app.mount("/static", StaticFiles(directory="static"), name="static")
-app = FastAPI(title="Pera Kafe API")
-app.add_middleware(CORSMiddleware, allow_origins=["*"],
-                   allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
-app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.get("/")
 # EKLENECEK KISIM BURASI
