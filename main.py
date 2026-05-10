5ad22bfe4ba3836411d3e11d9c7df22ef1c99faa
KoalAren <160577811+KoalAren@users.noreply.github.com>
2026-05-10 05:20:04 +0300

    png2

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ TOKEN_TTL = 3600 * 8                   # 8 saat
 app = FastAPI(title="Pera Kafe API")
 app.add_middleware(CORSMiddleware, allow_origins=["*"],
                    allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
-app.mount("/static", StaticFiles(directory="/static"), name="static")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 # EKLENECEK KISIM BURASI
